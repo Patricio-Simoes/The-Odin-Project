76adaedd260c36c4833e28c67f205370fd296970
patricio-simoes <code.psimoes@proton.me>
2023-11-12 19:04:52 +0000

    Fixed directories

--- Projects/rock-paper-scissors/JS/app.js
+++
@@ -1,6 +0,0 @@
-rangerViking.addEventListener("click", playPlayerPick);
-warriorViking.addEventListener("click", playPlayerPick);
-assassinViking.addEventListener("click", playPlayerPick);
-
-btnStartElement.addEventListener("click", startGame);
-playAgainButtonElement.addEventListener("click", startGame);
